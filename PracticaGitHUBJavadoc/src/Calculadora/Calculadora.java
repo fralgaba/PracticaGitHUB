858e96afc31dda1db86937ad57ab9790374fa0c7
francisco <francisco@MintEntornos>
2018-03-03 23:32:55 +0100

    Añade la documentación Javadoc
    Genera la documentación necesaria para la correcta utilizáción de la
    clase Calculadora.

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -1,24 +1,50 @@
 package Calculadora;
 
+/**
+ * @author Francisco Algaba
+ * @version 0.1
+ */
+
 public class Calculadora {
 
+	/**
+	 * Este atributo es un número real (primer operador)
+	 */
 	double op1;
+	/**
+	 * Este atributo es otro número real (segundo operador)
+	 */
 	double op2;
 	
-	
+	/**
+	 * Este constructor genera un objeto de la clase 
+	 * Calculadora con el que haremos distintas operaciones
+	 * @param op1 número real
+	 * @param op2 número real
+	 */
 	public Calculadora(double op1, double op2) {
 		this.op1 = op1;
 		this.op2 = op2;
 	}
 	
+	/**
+	 * @return devuelve el resultado de sumar op1 + op2
+	 */
 	double suma() {
 		return op1+op2;
 	}
 	
+	/**
+	 * @return devuelve el resultado de restar op1 - op2
+	 */
 	double resta() {
 		return op1-op2;
 	}
 	
+	/**
+	 * @return devuelve el resultado de dividir op1 / op2
+	 * @throws IllegalArgumentException si op2 = 0
+	 */
 	double division() {
 		if(op2==0) {
 			throw new IllegalArgumentException("El divisor es 0");
@@ -32,6 +58,9 @@ public class Calculadora {
 	//-Un n�mero "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
 	//As� 232^0 = 1; y 2^(-3)=1/2^3=1/8
 	
+	/**
+	 * @return devuelve el resultado de elevar op1 a op2.
+	 */
 	double potencia() {
 		
 		return Math.pow(op1, op2);
@@ -43,6 +72,10 @@ public class Calculadora {
 	//"operando1 = <op1>"
 	//"operando2 = <op2>"
 	
+	/**
+	 * @return devuelve el autor de la calculadora y el valor de op1 y op2
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Calculadora de Francisco Algaba:\n"
